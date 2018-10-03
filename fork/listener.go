4baf356b3ea30733deacf0134a4ed5df587ee135
Markus Tzoe <chou.marcus@gmail.com>
2018-10-03 12:14:41 +0800

    fork: refine listener
    Reload() uses File.Name() as filename fallback
    resolve address-in-use binding issue:
    	tcp -> tcp4
    	udp -> udp6

--- fork/listener.go
+++ fork/listener.go
@@ -41,28 +41,56 @@ func init() {
 	}
 }
 
+func resolveAddr(network, address string) (addr net.Addr, err error) {
+	switch network {
+	case "unix", "unixpacket", "unixgram":
+		return net.ResolveUnixAddr(network, address)
+	default:
+	}
+
+	ip, port, err := func(network, address string) (iaddr *net.IPAddr, portnum int, err error) {
+		switch network {
+		case "tcp", "tcp4", "tcp6", "udp", "udp4", "udp6":
+			host, port, err := net.SplitHostPort(address)
+			if err != nil {
+				return iaddr, portnum, err
+			}
+			portnum, err = net.LookupPort(network, port)
+			if err != nil {
+				return iaddr, portnum, err
+			}
+			iaddr, err = net.ResolveIPAddr("ip", host)
+			if err != nil {
+				return iaddr, portnum, err
+			}
+		default:
+			err = fmt.Errorf("network `%s' unsupported", network)
+		}
+		return
+	}(network, address)
+	if err != nil {
+		return nil, err
+	}
+	if ip.IP == nil {
+		if network != "" && network[len(network)-1] == '6' {
+			ip.IP = net.IPv6zero
+		} else {
+			ip.IP = net.IPv4zero
+		}
+	}
+	switch network {
+	case "tcp", "tcp4", "tcp6":
+		addr = &net.TCPAddr{IP: ip.IP, Port: port, Zone: ip.Zone}
+	case "udp", "udp4", "udp6":
+		addr = &net.UDPAddr{IP: ip.IP, Port: port, Zone: ip.Zone}
+	}
+	return
+}
+
 // Listen reloads inherited listener or creates a new one; only TCP & Unix network supported.
 // Listen should be called instead of normal `net.Listen` calls.
 func Listen(network, address string) (l ReloadableListener, err error) {
-	var addr net.Addr
-	switch network {
-	case "tcp", "tcp4":
-		tcpaddr, err1 := net.ResolveTCPAddr(network, address)
-		if tcpaddr != nil && tcpaddr.IP == nil {
-			tcpaddr.IP = net.IPv4zero
-		}
-		addr, err = tcpaddr, err1
-	case "tcp6":
-		tcpaddr, err1 := net.ResolveTCPAddr(network, address)
-		if tcpaddr != nil && tcpaddr.IP == nil {
-			tcpaddr.IP = net.IPv6zero
-		}
-		addr, err = tcpaddr, err1
-	case "unix", "unixpacket":
-		addr, err = net.ResolveUnixAddr(network, address)
-	default:
-		err = fmt.Errorf("network `%s' unsupported", network)
-	}
+	addr, err := resolveAddr(network, address)
 	if err != nil {
 		return nil, err
 	}
@@ -73,18 +101,24 @@ func Listen(network, address string) (l ReloadableListener, err error) {
 		if file == nil {
 			return nil, fmt.Errorf("unable to create listener file %s", filename)
 		}
+		defer file.Close()
 		ln, err = net.FileListener(file)
 		if err != nil {
 			return nil, err
 		}
-		defer file.Close()
-	} else {
-		ln, err = net.Listen(network, address)
-		if err != nil {
-			return
+		if l, ok = ln.(ReloadableListener); !ok {
+			return nil, fmt.Errorf("listener %s not reloadable", filename)
 		}
+		return l, nil
 	}
-	l = ln.(ReloadableListener)
+	switch network {
+	case "tcp", "tcp4":
+		l, err = net.ListenTCP("tcp4", addr.(*net.TCPAddr))
+	case "tcp6":
+		l, err = net.ListenTCP("tcp6", addr.(*net.TCPAddr))
+	case "unix", "unixpacket":
+		l, err = net.ListenUnix(network, addr.(*net.UnixAddr))
+	}
 	return
 }
 
@@ -108,13 +142,13 @@ func Reload(reloadables ...Reloadable) (int, error) {
 		if err != nil {
 			return -1, err
 		}
-		switch r := re.(type) { // envListeners[f.Name()] = i + 3
+		switch r := re.(type) {
 		case ReloadableListener:
 			envListeners[filename(r.Addr())] = i + 3
 		case ReloadablePacketConn:
 			envListeners[filename(r.LocalAddr())] = i + 3
 		default:
-			continue // ignore
+			envListeners[f.Name()] = i + 3
 		}
 		files = append(files, f.Fd())
 	}
@@ -138,33 +172,13 @@ func filename(laddr net.Addr) string {
 	return laddr.Network() + ":" + laddr.String() + "->"
 }
 
+// ListenPacket reloads inherited net.PacketConn or creates a new one; only UDP & Unix(gram) network supported.
+// ListenPacket should be called instead of normal `net.ListenPacket` calls.
 func ListenPacket(network, address string) (c ReloadablePacketConn, err error) {
-	var addr net.Addr
-	switch network {
-	case "udp", "udp4":
-		laddr, err := net.ResolveUDPAddr(network, address)
-		if err != nil {
-			return nil, err
-		}
-		if laddr != nil && laddr.IP == nil {
-			laddr.IP = net.IPv4zero
-		}
-		addr = laddr
-	case "udp6":
-		laddr, err := net.ResolveUDPAddr(network, address)
-		if err != nil {
-			return nil, err
-		}
-		if laddr != nil && laddr.IP == nil {
-			laddr.IP = net.IPv6zero
-		}
-		addr = laddr
-	case "unix", "unixgram":
-		addr, err = net.ResolveUnixAddr(network, address)
-	default:
-		err = fmt.Errorf("network `%s' unsupported", network)
+	addr, err := resolveAddr(network, address)
+	if err != nil {
+		return nil, err
 	}
-
 	filename := filename(addr)
 	var conn net.PacketConn
 	if fd, ok := inheritedFDs[filename]; ok {
@@ -172,17 +186,24 @@ func ListenPacket(network, address string) (c ReloadablePacketConn, err error) {
 		if file == nil {
 			return nil, fmt.Errorf("unable to create conn file %s", filename)
 		}
+		defer file.Close()
 		conn, err = net.FilePacketConn(file)
 		if err != nil {
 			return nil, err
 		}
-		defer file.Close()
-	} else {
-		conn, err = net.ListenPacket(network, address)
-		if err != nil {
-			return
+		if c, ok = conn.(ReloadablePacketConn); !ok {
+			return nil, fmt.Errorf("packet conn %s not reloadable", filename)
 		}
+		return c, nil
+
 	}
-	c = conn.(ReloadablePacketConn)
+	switch network {
+	case "udp", "udp4":
+		c, err = net.ListenUDP("udp4", addr.(*net.UDPAddr))
+	case "udp6":
+		c, err = net.ListenUDP("udp6", addr.(*net.UDPAddr))
+	case "unix", "unixgram":
+		c, err = net.ListenUnixgram("unixgram", addr.(*net.UnixAddr))
+	}
 	return
 }
